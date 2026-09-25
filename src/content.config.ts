d2da409f0cf023a88306b627c10e0fa5ca9817ae
rajsite <rajsite@users.noreply.github.com>
2026-09-24 22:19:05 -0500

    More astro slug cleanup

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,12 +3,17 @@ import { defineCollection } from 'astro:content';
 import { z } from 'astro/zod';
 
 const blogCollection = defineCollection({
-    loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
+    loader: glob({
+        pattern: '**/*.{md,mdx}',
+        base: './src/content/blog',
+        generateId: ({ data }) => data.permalink as string,
+    }),
     schema: z.object({
         title: z.string(),
         description: z.string(),
         date: z.date(),
         image: z.string().optional(),
+        permalink: z.string(),
     }),
 });
 
